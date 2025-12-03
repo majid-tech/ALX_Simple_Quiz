b3488dcdbcf0a5632ffbd210b00cf14cf0ce9d0a
majid-tech <majidmohammed044@gmail.com>
2025-12-03 21:23:30 +0000

    create quiz app

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ function checkAnswer() {
 
     const feedback = document.getElementById("feedback")
 
-    if(userAnswer == correctAnswer){
+    if (userAnswer === correctAnswer){
         feedback.textContent = "Correct! Well done."
     } else {
         feedback.textContent = "That's incorrect. Try again!"
