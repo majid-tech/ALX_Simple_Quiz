88008287668afcdc5a04d932229f19beb3fb4a06
majid-tech <majidmohammed044@gmail.com>
2025-12-03 21:21:22 +0000

    create quiz app

---
+++ quiz.js
@@ -0,0 +1,18 @@
+
+function checkAnswer() {
+    const correctAnswer = "4"
+
+    userAnswer = document.querySelector('input[type="radio"]:checked').value
+
+    const feedback = document.getElementById("feedback")
+
+    if(userAnswer == correctAnswer){
+        feedback.textContent = "Correct! Well done."
+    } else {
+        feedback.textContent = "That's incorrect. Try again!"
+    }
+}
+
+const submitAnswerbtn = document.getElementById("submit-answer")
+
+submitAnswerbtn.addEventListener("click", checkAnswer)
